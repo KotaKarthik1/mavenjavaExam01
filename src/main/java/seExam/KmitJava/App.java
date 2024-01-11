a45724752671308578c63d5462f27ea15403e159
KotaKarthik1 <karthikkotaps1@gmail.com>
2024-01-11 11:09:42 +0530

    first commit

---
+++ src/main/java/seExam/KmitJava/App.java
@@ -0,0 +1,13 @@
+package seExam.KmitJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/seExam/KmitJava/AppTest.java
@@ -0,0 +1,20 @@
+package seExam.KmitJava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
